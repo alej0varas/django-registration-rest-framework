f52fdc59a3e4b8a833de69468d50f03bd1fe1deb
CoreyTrombley <corey.trombley33@gmail.com>
2017-04-01 20:14:24 -0400

    updated django version and urls

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(name='django-registration-rest-framework',
       packages=find_packages(),
       requires=['djangorestframework'],
       test_suite='tests.runtests.runtests',
-      install_requires=['djangorestframework==2.3.10'],
+      install_requires=[
+          'djangorestframework==2.3.10',
+          'django==1.10.0'
+      ],
       tests_require=[
           'mock==1.0.1',
           'Django>=1.5',
